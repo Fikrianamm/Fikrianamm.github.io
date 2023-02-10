15d8772feabb10f2375b5bbf8b55e72d51b950bb
Fikrianamm <fikritaufiqulanam1@gmail.com>
2023-02-10 16:19:20 +0700

    first commit

---
+++ src/dist/js/script.js
@@ -0,0 +1,22 @@
+const hamburger = document.querySelector(".hamburger");
+const doctitle = document.title;
+const menu = document.querySelector('.menu');
+
+hamburger.addEventListener('click',()=>{
+    hamburger.classList.toggle('is-active')
+    menu.classList.toggle('menu-active')
+});
+
+window.addEventListener('blur',()=>{
+    document.title = "Come Back :(";
+});
+
+window.addEventListener('focus',()=>{
+    document.title = doctitle;
+});
+
+window.addEventListener('scroll',()=>{
+    hamburger.classList.remove('is-active')
+    menu.classList.remove('menu-active')
+    
+});
