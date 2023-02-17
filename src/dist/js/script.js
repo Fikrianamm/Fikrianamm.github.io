8a5d2c9caae2f657eaff824c5dc1e38a2c1deda9
Fikrianamm <fikritaufiqulanam1@gmail.com>
2023-02-17 14:41:54 +0700

    fix date and time

--- src/dist/js/script.js
+++ src/dist/js/script.js
@@ -2,6 +2,7 @@ const hamburger = document.querySelector('.hamburger');
 const doctitle = document.title;
 const menu = document.querySelector('.menu');
 
+
 hamburger.addEventListener('click',()=>{
     hamburger.classList.toggle('is-active')
     menu.classList.toggle('menu-active')
@@ -19,3 +20,24 @@ window.addEventListener('scroll',()=>{
     hamburger.classList.remove('is-active')
     menu.classList.remove('menu-active')
 });
+
+
+setInterval(()=>{
+    const day = document.querySelector('.day');
+    const date = document.querySelector('.date');
+    const newDate = new Date();
+    const hari = newDate.getDay();
+    const strHari = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+    const tanggal = newDate.getDate();
+    const bulan = newDate.getMonth();
+    const strBulan = ['Dec','Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov'];
+    const tahun = newDate.getFullYear();
+    const jam = newDate.getHours();
+    const menit = newDate.getMinutes();
+    const detik = newDate.getSeconds();
+
+
+    day.innerHTML = strHari[hari];
+    date.innerHTML = tanggal + " " + strBulan[bulan] + " " + tahun + " | " + jam + "." + menit + "." + detik;    
+},1000);
+    
